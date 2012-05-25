11ed13d3e363df625314e56e4cc20e0df782d1bd
Lunch on 4 Wheels <lunchonfourwheels@gmail.com>
2012-05-24 21:48:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchonfourwheels2::Application.config.secret_token = 'b9d47853d7738f797b09c168560114ca93bd16d43d13a59d79a2764243f3ece7152eadf7130ac2e4f57336f96fb5e7c62a90575e24fec3a944460d2cd196b384'
