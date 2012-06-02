b433adb6a546c8d636a6c76625567948fb9a543a
Lunch on 4 Wheels <lunchonfourwheels@gmail.com>
2012-06-02 11:31:00 -0700

    Add find by date range function to Truck object, with tests

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -3,12 +3,39 @@ class Truck < ActiveRecord::Base
 
   def find_stops_by_date( date=Date.today )
 
-    wday_string = date.strftime("%A") + "s"
-    date_string = date.to_s
+    find_stops_by_date_range( date, date )
 
-    @truck = Truck.find(id)
-    return @truck.stops.where( 'day LIKE ? OR day LIKE ?', wday_string,
-      date_string )
+  end
+
+  def find_stops_by_date_range( first_date, second_date )
+
+    # first make sure that the second date actually comes after the first
+    if (second_date >= first_date)
+
+      # create a string to build up our where clause
+      where_array = []
+
+      # iterate over the range between the two dates
+      first_date.upto(second_date) do | day |
+
+        # grab a string to match the weekday and a string to match the date
+        wday_string = day.strftime("%A") + "s"
+        date_string = day.to_s
+
+        where_array += [ "\"day\" LIKE \"#{wday_string}\" OR \"day\" LIKE \"#{date_string}\"" ]
+
+      end
+
+      # grab the truck object that were have instantiated
+      @truck = Truck.find(id)
+
+      return @truck.stops.where( where_array.join( sep=" OR ") )
+
+    else
+       
+      return nil
+
+    end
 
   end
 
--- spec/models/truck_spec.rb
+++ spec/models/truck_spec.rb
@@ -5,28 +5,57 @@ describe Truck do
   before :each do 
     @marination_truck = Truck.find_by_name("Marination Mobile")
     @pais_truck = Truck.find_by_name("Pai's")
-  end
+  end  # before :each
 
   describe "#new" do
     it "returns a Truck object" do
       @marination_truck.should be_an_instance_of Truck
     end
-  end
+  end  #new 
 
   describe "#find_stops_by_date" do
 
-    it "takes a date paramemter and returns a Stop object" do
-      stop = @marination_truck.find_stops_by_date( 
-        Date.new(2012, 5, 31) ).first
-      stop.location.address.should eql "2401 Utah Ave. S"
+    it "takes a date parameter and returns an array of stops" do
+      stops = @marination_truck.find_stops_by_date( Date.today )
+      stops.class.should eql ActiveRecord::Relation
     end
 
-    it "should not simply match the first item in the list" do
-      stop = @pais_truck.find_stops_by_date( 
-        Date.new(2012, 6, 1) ).first
-      stop.location.address.should_not eql "1126 Harrison St."
+    it "takes a date paramemter at least one stop for that date" do
+      stops = @marination_truck.find_stops_by_date( 
+        Date.new(2012, 5, 31) )
+      stops.first.location.address.should eql "2401 Utah Ave. S"
     end
 
-  end
+    it "doesn't just match the first item in the list" do
+      stops = @pais_truck.find_stops_by_date( 
+        Date.new(2012, 6, 1) )
+      stops.first.location.address.should_not eql "1126 Harrison St."
+    end
+
+  end  # describe find_stops_by_date
+
+  describe "#find_stops_by_date_range" do
+   
+
+    it "returns a nil object if the second date is earlier than the first" do
+      stops = @pais_truck.find_stops_by_date_range(
+        Date.new(2012, 5, 29), Date.new(2012, 5, 27) )
+      stops.should eql nil
+    end
+
+    it "takes two date parameters and returns an array of Stop objects" do
+      stops = @pais_truck.find_stops_by_date_range(
+        Date.new(2012, 5, 29), Date.new(2012, 6, 1) )
+      stops[0].location.address.should eql "1126 Harrison St."
+      stops[1].location.address.should eql "2401 Utah Ave. S"
+    end
+
+    it "takes the same date twice and returns Stops for that day" do
+      stops = @pais_truck.find_stops_by_date_range(
+        Date.new(2012, 6, 1), Date.new(2012, 6, 1) )
+      stops.first.location.address.should eql "2401 Utah Ave. S"
+    end
+
+  end  # describe find_stops_by_date_range
 
 end
