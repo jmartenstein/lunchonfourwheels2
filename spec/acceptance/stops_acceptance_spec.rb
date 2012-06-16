4e48d28d0540c3008daf609497e664fda6c38e7a
Lunch on 4 Wheels <lunchonfourwheels@gmail.com>
2012-06-16 16:07:15 -0700

    Update truck and stop acceptance test

--- spec/acceptance/stops_acceptance_spec.rb
+++ spec/acceptance/stops_acceptance_spec.rb
@@ -4,18 +4,29 @@ feature "Stops", %q{
 } do
 
   background do
-    @marination_truck = Truck.find_by_id(1)
+    @truck1         = Truck.create( :name => "Truck 1" )
+    @neighborhood   = Neighborhood.create( :name => "Neighborhood" ) 
+    @location1      = Location.create( 
+                        :address => "1 Address St.",
+                        :neighborhood_id => 1 
+                      )
+    @stop1          = Stop.create(
+                        :truck_id => 1,
+                        :location_id => 1,
+                        :day => Date.today.to_s,
+                        :hours => "Hours"
+                      )
   end
 
   scenario "Stop shows truck name instead of id" do
     visit '/stops/1'
-    page.should have_content @marination_truck.name
+    page.should have_content @truck1.name
   end
 
   scenario "Stop edit page shows truck name and day info" do
     visit '/stops/1/edit'
-    page.should have_content @marination_truck.name
-    page.should have_content "Mondays"
+    page.should have_content @truck1.name
+    page.should have_content Date.today.to_s
   end
 
 end  # feature Stops
--- spec/acceptance/trucks_acceptance_spec.rb
+++ spec/acceptance/trucks_acceptance_spec.rb
@@ -4,18 +4,26 @@ feature "Trucks", %q{
 } do
 
   background do
+    @truck1 = Truck.create( :name => "Truck 1" )
+    @location1 = Location.create( :address => "Address 1" )
+    @stop1 = Stop.create( 
+      :truck_id => 1,
+      :location_id => 1,
+      :day => Date.today.to_s,
+      :hours => "Hours"
+    )
   end
 
   scenario "Add a new stop to the Marination Mobile page" do
     visit '/trucks/1'
     page.should have_link "New Stop"
     click_link "New Stop"
-    page.shoud have_content "Marination"
+    page.shoud have_content "Truck 1"
   end
 
   scenario "Show page for first truck (Marination Mobile)" do
     visit '/trucks/1'
-    page.should have_content "Marination Mobile"
+    page.should have_content "Truck 1"
   end
 
   scenario "Confirm 'Show' link for first Marination stop" do
