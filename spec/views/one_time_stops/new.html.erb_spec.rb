5ca27ae7257981fc913212ce73e801f23601286b
Lunch on 4 Wheels <lunchonfourwheels@gmail.com>
2012-06-22 23:13:42 -0700

    Tweak views for OneTimeStop objects and associated test

--- spec/views/one_time_stops/new.html.erb_spec.rb
+++ spec/views/one_time_stops/new.html.erb_spec.rb
@@ -16,8 +16,8 @@ describe "one_time_stops/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => one_time_stops_path, :method => "post" do
-      assert_select "input#one_time_stop_truck_id", :name => "one_time_stop[truck_id]"
-      assert_select "input#one_time_stop_location_id", :name => "one_time_stop[location_id]"
+      assert_select "select#one_time_stop_truck_id", :name => "one_time_stop[truck_id]"
+      assert_select "selec#one_time_stop_location_id", :name => "one_time_stop[location_id]"
       assert_select "input#one_time_stop_start_time", :name => "one_time_stop[start_time]"
       assert_select "input#one_time_stop_end_time", :name => "one_time_stop[end_time]"
       assert_select "input#one_time_stop_cancelled_flag", :name => "one_time_stop[cancelled_flag]"
--- spec/views/one_time_stops/show.html.erb_spec.rb
+++ spec/views/one_time_stops/show.html.erb_spec.rb
@@ -4,18 +4,29 @@ describe "one_time_stops/show" do
   before(:each) do
     @one_time_stop = assign(:one_time_stop, stub_model(OneTimeStop,
       :truck_id => 1,
-      :location_id => 2,
+      :location_id => 1,
       :start_time => 3,
       :end_time => 4,
       :cancelled_flag => false
+    ))
+    @one_time_stop.truck = assign(:truck, stub_model(Truck,
+      :name => "Truck Name"
+    ))
+    @one_time_stop.location = assign(:location, stub_model(Location,
+      :address => "Location Address",
+      :neighborhood_id => 1
+    ))
+    @one_time_stop.location.neighborhood = assign(:neighborhood, stub_model(Neighborhood,
+      :name => "Neighborhood Name"
     ))
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    rendered.should match(/2/)
+    rendered.should match(/Truck Name/)
+    rendered.should match(/Neighborhood Name/)
+    rendered.should match(/Location Address/)
     rendered.should match(/3/)
     rendered.should match(/4/)
     rendered.should match(/false/)
