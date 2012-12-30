572813a1c50cd7f91bc0e1a534ea9676423c69d7
Lunch on 4 Wheels <lunchonfourwheels@gmail.com>
2012-12-29 20:26:17 -0800

    Add lat long to Location class

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,53 @@
+require 'net/http'
+require 'json'
+
 class Location < ActiveRecord::Base
   belongs_to :neighborhood
   has_many :one_time_stops
   validates_presence_of :address
+
+  after_initialize :default_values
+
+  def default_values
+
+    #http_string = get_lookup_string()
+
+    #uri = URI.parse(http_string)
+    #response = 
+    #Net::HTTP.get_response("example.com","/?search=thing&format=json")
+
+    latlong_array = [0, 0]
+    if (!self.address.nil?)
+      latlong_array = lookup_lat_long()
+    end
+
+    self.latitude = latlong_array[0]
+    self.longitude = latlong_array[1]
+
+  end
+
+  def lookup_lat_long( )
+
+    # swap out a "+" for a space, so we get a valid URL
+    if (!self.address.nil?)
+      subbed_address = self.address.gsub(' ', '+')
+    else
+      subbed_address = ""
+    end
+
+    #build the URL and make the HTTP request
+    response = Net::HTTP.get_response("maps.googleapis.com","/maps/api/geocode/json?address="+subbed_address+"&sensor=false")
+
+    # parse the JSON response
+    parsed = JSON.parse(response.body)
+
+    lat = parsed['results'][0]['geometry']['location']['lat']
+    long = parsed['results'][0]['geometry']['location']['lng']
+
+    latlong = [lat, long]
+
+    return latlong
+
+  end
+
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,7 +4,8 @@ describe Location do
 
   before :each do
     @location0 = Location.new()
-    @location1 = Location.new( :address => "1 Location St." )
+    @location1 = Location.new( :address => "1126 Harrison Street, Seattle, WA" )
+    @location2 = Location.new( :address => "2401 Utah Ave. S, Seattle, WA" )
   end
 
   it "is a location" do
@@ -18,6 +19,22 @@ describe Location do
   it "is invalid without an address" do
     @location0.should have(1).error_on(:address)
     @location0.should_not be_valid
+  end
+
+  it "can lookup a lat long with an address" do
+    loc_array = @location1.lookup_lat_long()
+    loc_array[0].should == 47.6220465
+    loc_array[1].should == -122.33502360
+  end
+
+  it "matches '1126 Harrison St. with a latitude and longitude" do
+    @location1.latitude.should == 47.6220465
+    @location1.longitude.should == -122.33502360
+  end
+
+  it "matches '2401 Utah Ave. S' with a latitude and longitude" do
+    @location2.latitude.should == 47.58046290
+    @location2.longitude.should == -122.33589650
   end
 
 end
